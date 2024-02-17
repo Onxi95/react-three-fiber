65f793ed9b521445a5b3067b7d1f3749220b6fe9
Paweł Sowa <pawel@the-sowa.com>
2024-02-17 17:48:34 +0100

    refactor: add orbit controls from drei

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -1,11 +1,6 @@
-import {
-  useFrame,
-  extend,
-  useThree,
-  type ReactThreeFiber,
-} from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
+import { useFrame } from "@react-three/fiber";
 import { useRef } from "react";
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import type {
   BufferGeometry,
   Group,
@@ -14,20 +9,6 @@ import type {
   NormalBufferAttributes,
   Object3DEventMap,
 } from "three";
-
-extend({ OrbitControls });
-
-declare global {
-  // eslint-disable-next-line @typescript-eslint/no-namespace
-  namespace JSX {
-    interface IntrinsicElements {
-      orbitControls: ReactThreeFiber.Object3DNode<
-        OrbitControls,
-        typeof OrbitControls
-      >;
-    }
-  }
-}
 
 export const Experience = () => {
   const knotRef = useRef<Mesh<
@@ -38,24 +19,16 @@ export const Experience = () => {
 
   const groupRef = useRef<Group<Object3DEventMap> | null>(null);
 
-  const { camera, gl } = useThree();
-
-  useFrame(({ clock, camera }, delta) => {
+  useFrame((_, delta) => {
     knotRef.current!.rotation.y += delta;
     groupRef.current!.rotation.y += delta * 0.5;
     groupRef.current!.rotation.x += delta * 0.5;
     groupRef.current!.rotation.z += delta * 0.5;
-
-    const angle = clock.elapsedTime;
-
-    camera.position.x = Math.sin(angle) * 8;
-    camera.position.z = Math.cos(angle) * 8;
-    camera.lookAt(0, 0, 0);
   });
 
   return (
     <>
-      <orbitControls args={[camera, gl.domElement]} />
+      <OrbitControls />
       <directionalLight position={[1, 2, 3]} intensity={4} />
       <ambientLight intensity={1} />
       <group ref={groupRef}>
