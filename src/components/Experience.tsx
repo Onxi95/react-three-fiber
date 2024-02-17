5c08cfbb1b6e283c063698cbb846f989b0e34317
Paweł Sowa <pawel@the-sowa.com>
2024-02-17 18:09:51 +0100

    feat: add gizmo controls

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -1,4 +1,4 @@
-import { OrbitControls } from "@react-three/drei";
+import { OrbitControls, TransformControls } from "@react-three/drei";
 import { useFrame } from "@react-three/fiber";
 import { useRef } from "react";
 import type {
@@ -10,6 +10,12 @@ import type {
   Object3DEventMap,
 } from "three";
 
+type SphereMesh = Mesh<
+  BufferGeometry<NormalBufferAttributes>,
+  Material | Material[],
+  Object3DEventMap
+>;
+
 export const Experience = () => {
   const knotRef = useRef<Mesh<
     BufferGeometry<NormalBufferAttributes>,
@@ -18,24 +24,29 @@ export const Experience = () => {
   > | null>(null);
 
   const groupRef = useRef<Group<Object3DEventMap> | null>(null);
+  const sphereMeshRef = useRef<SphereMesh | null>(null);
 
-  useFrame((_, delta) => {
-    knotRef.current!.rotation.y += delta;
-    groupRef.current!.rotation.y += delta * 0.5;
-    groupRef.current!.rotation.x += delta * 0.5;
-    groupRef.current!.rotation.z += delta * 0.5;
-  });
+  // useFrame((_, delta) => {
+  // rotate animation
+  // knotRef.current!.rotation.y += delta;
+  // groupRef.current!.rotation.y += delta * 0.5;
+  // groupRef.current!.rotation.x += delta * 0.5;
+  // groupRef.current!.rotation.z += delta * 0.5;
+  // });
 
   return (
     <>
-      <OrbitControls />
+      <OrbitControls makeDefault />
       <directionalLight position={[1, 2, 3]} intensity={4} />
       <ambientLight intensity={1} />
       <group ref={groupRef}>
-        <mesh position-x={-2}>
+        <mesh position-x={-2} ref={sphereMeshRef}>
           <sphereGeometry />
           <meshStandardMaterial color="yellow" />
         </mesh>
+        <TransformControls
+          object={sphereMeshRef as React.MutableRefObject<SphereMesh>}
+        />
         <mesh position-x={2} position-y={1} ref={knotRef}>
           <torusGeometry />
           <meshStandardMaterial />
