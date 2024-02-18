2f715b303544dec41d8cb3211a1c8c521145c11a
Paweł Sowa <pawel@the-sowa.com>
2024-02-18 09:14:27 +0100

    feat: add `<Perf />`

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import { Canvas } from "@react-three/fiber";
 import { Experience } from "./components/Experience";
+import { Perf } from "r3f-perf";
 
 function App() {
   return (
     <Canvas>
+      <Perf position="top-left" />
       <Experience />
     </Canvas>
   );
