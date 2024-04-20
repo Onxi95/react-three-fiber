c81f279067bc366b787d575c9700a2f7dcec5ec1
Paweł Sowa <pawel@the-sowa.com>
2024-04-20 10:02:30 +0200

    feat: add Ridgid Bodies

--- src/components/PhysicsExperience.tsx
+++ src/components/PhysicsExperience.tsx
@@ -1,24 +1,37 @@
 import { OrbitControls } from "@react-three/drei";
+import { Physics, RigidBody } from "@react-three/rapier";
 
 export const PhysicsExperience = () => {
   return (
     <>
       <OrbitControls makeDefault />
-      <directionalLight castShadow position={[1, 2, 3]} intensity={1.5} />
+      <directionalLight
+        castShadow
+        position={[1, 2, 3]}
+        intensity={1.5}
+        shadow-mapSize={2048}
+      />
       <ambientLight intensity={0.5} />
-      <mesh castShadow position={[-2, 2, 0]}>
-        <sphereGeometry />
-        <meshStandardMaterial color="orange" />
-      </mesh>
-      <mesh castShadow receiveShadow position={[2, 2, 0]}>
-        <boxGeometry />
-        <meshStandardMaterial color="green" />
-      </mesh>
-      <mesh receiveShadow position-y={-0.5}>
-        <boxGeometry args={[10, 0.5, 10]} />
-        <meshStandardMaterial color="greenyellow" />
-      </mesh>
-      
+      <Physics>
+        <RigidBody>
+          <mesh castShadow position={[-2, 2, 0]}>
+            <sphereGeometry />
+            <meshStandardMaterial color="orange" />
+          </mesh>
+        </RigidBody>
+        <RigidBody>
+          <mesh castShadow receiveShadow position={[2, 2, 0]}>
+            <boxGeometry />
+            <meshStandardMaterial color="green" />
+          </mesh>
+        </RigidBody>
+        <RigidBody type="fixed">
+          <mesh receiveShadow position-y={-0.5}>
+            <boxGeometry args={[15, 0.5, 15]} />
+            <meshStandardMaterial color="greenyellow" />
+          </mesh>
+        </RigidBody>
+      </Physics>
     </>
   );
 };
