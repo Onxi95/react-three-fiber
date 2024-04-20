ab5a81312cb69653067dfb0176b3ac8245ecda5f
Paweł Sowa <pawel@the-sowa.com>
2024-04-20 09:54:52 +0200

    feat: add scene

--- src/components/PhysicsExperience.tsx
+++ src/components/PhysicsExperience.tsx
@@ -1,9 +1,24 @@
-import { Environment, OrbitControls } from "@react-three/drei";
+import { OrbitControls } from "@react-three/drei";
 
 export const PhysicsExperience = () => {
   return (
-    <Environment background preset="sunset">
+    <>
       <OrbitControls makeDefault />
-    </Environment>
+      <directionalLight castShadow position={[1, 2, 3]} intensity={1.5} />
+      <ambientLight intensity={0.5} />
+      <mesh castShadow position={[-2, 2, 0]}>
+        <sphereGeometry />
+        <meshStandardMaterial color="orange" />
+      </mesh>
+      <mesh castShadow receiveShadow position={[2, 2, 0]}>
+        <boxGeometry />
+        <meshStandardMaterial color="green" />
+      </mesh>
+      <mesh receiveShadow position-y={-0.5}>
+        <boxGeometry args={[10, 0.5, 10]} />
+        <meshStandardMaterial color="greenyellow" />
+      </mesh>
+      
+    </>
   );
 };
