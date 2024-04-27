3efe99ce3a5e74b1a6e1bb07ee0c356eb1c637d1
Paweł Sowa <pawel@the-sowa.com>
2024-04-27 10:16:05 +0200

    chore: add ssl plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
 import { defineConfig } from "vite";
+import basicSsl from "@vitejs/plugin-basic-ssl";
 import react from "@vitejs/plugin-react-swc";
 import { TanStackRouterVite } from "@tanstack/router-vite-plugin";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), TanStackRouterVite()],
+  server: {
+    host: true,
+    https: true,
+  },
+  plugins: [react(), TanStackRouterVite(), basicSsl()],
 });
